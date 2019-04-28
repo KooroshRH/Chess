121b123e18580680edd7141ecbbe1e49ea53e2d5
KoroshRH <46434958+KoroshRH@users.noreply.github.com>
2019-04-28 19:11:57 +0430

    move method in Horse class is finished.

--- src/Horse.java
+++ src/Horse.java
@@ -6,6 +6,11 @@ public class Horse extends Piece {
 
     @Override
     boolean move(int width, int height, char[][] map) {
-        return false;
+        if ((Math.abs(width - this.width) == 2 && Math.abs(height - this.height) == 1) && (Math.abs(width - this.width) == 1 || Math.abs(height - this.height) == 2)){
+            return isMoving(width, height, map);
+        } else {
+            System.out.println("This piece can't move to your selected place!!");
+            return false;
+        }
     }
 }
