869d12b7c2281c7d4cb8b48e02aae79150553510
KoroshRH <46434958+KoroshRH@users.noreply.github.com>
2019-04-29 14:48:38 +0430

    Soldier moving method debugged.

--- src/Soldier.java
+++ src/Soldier.java
@@ -1,5 +1,6 @@
 public class Soldier extends Piece {
     private boolean isFirst;
+
     public Soldier(int width, int height, boolean isIn, char color, int ID) {
         super(width, height, isIn, color, ID);
         isFirst = true;
@@ -7,22 +8,44 @@ public class Soldier extends Piece {
 
     @Override
     boolean move(int width, int height, char[][] map) {
-        if (width == this.width){
-            if (height - this.height == 2 && isFirst){
-                isFirst = false;
-                return isMoving(width, height, map);
-            } else if (height - this.height == 1){
-                return isMoving(width, height, map);
+        if (color == 'W') {
+            if (width == this.width) {
+                if (height - this.height == 2 && isFirst) {
+                    isFirst = false;
+                    return isMoving(width, height, map);
+                } else if (height - this.height == 1) {
+                    return isMoving(width, height, map);
+                } else {
+                    System.out.println("This piece can't move to your selected place!!");
+                    return false;
+                }
             } else {
-                System.out.println("This piece can't move to your selected place!!");
-                return false;
+                if (map[height-1][width-1] == 'B' && (height - this.height == 1 && (Math.abs(this.width-width) == 1))) {
+                    return isMoving(width, height, map);
+                } else {
+                    System.out.println("This piece can't move to your selected place!!");
+                    return false;
+                }
             }
         } else {
-            if (map[height-1][width-1] == color){
-                System.out.println("This place is filled by your own piece!!");
-                return false;
+            if (width == this.width) {
+                if (this.height - height == 2 && isFirst) {
+                    isFirst = false;
+                    return isMoving(width, height, map);
+                } else if (this.height - height == 1) {
+                    return isMoving(width, height, map);
+                } else {
+                    System.out.println("This piece can't move to your selected place!!");
+                    return false;
+                }
             } else {
-                return isMoving(width, height, map);
+                if (map[height-1][width-1] == 'W' && (this.height-height == 1 && (Math.abs(this.width-width) == 1))) {
+                    return isMoving(width, height, map);
+                } else {
+                    System.out.println(54);
+                    System.out.println("This piece can't move to your selected place!!");
+                    return false;
+                }
             }
         }
     }
