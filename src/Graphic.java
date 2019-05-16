4034d027bf7971d8c69d6abf09035c555e85cd55
KoroshRH <46434958+KoroshRH@users.noreply.github.com>
2019-05-16 19:25:59 +0430

    Now the pieces can move over the board.

--- src/Graphic.java
+++ src/Graphic.java
@@ -1,15 +1,25 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.File;
 
-public class Graphic {
+public class Graphic implements MouseListener {
+    private boolean clicked;
+    private char turn;
+    private Piece clickedPiece;
     private JFrame mainFrame;
+    private JPanel mainPanel;
     private JButton[][] map;
 
     public Graphic(){
+        clickedPiece = null;
         mainFrame = new JFrame();
+        mainPanel = new JPanel();
         map = new JButton[8][8];
+        clicked = false;
+        turn = 'W';
     }
 
     public void game(){
@@ -17,11 +27,6 @@ public class Graphic {
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setSize(new Dimension(700, 700));
         mainFrame.setResizable(false);
-        JPanel mainPanel = new JPanel();
-        JPanel whiteOut = new JPanel();
-        JPanel blackOut = new JPanel();
-        whiteOut.setSize(new Dimension(500, 900));
-        mainPanel.setPreferredSize(new Dimension(500, 500));
         mainFrame.add(mainPanel, BorderLayout.CENTER);
         mainPanel.setLayout(new GridLayout(8, 8));
         makePieces('W');
@@ -34,12 +39,15 @@ public class Graphic {
                 System.out.println(e.getMessage());
             }
             piece.setFocusable(false);
+            piece.addMouseListener(this);
             map[piece.getMyHeight()][piece.getMyWidth()] = piece;
         }
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++){
                 if (!(map[i][j] instanceof Piece)){
-                    map[i][j] = new JButton();
+                    JButton btt = new JButton();
+                    btt.addMouseListener(this);
+                    map[i][j] = btt;
                 }
             }
         }
@@ -49,7 +57,7 @@ public class Graphic {
 
     private void makePieces(char color){
         if (color == 'B') {
-            for (int i = 1; i <= 8; i++) {
+            for (int i = 0; i < 8; i++) {
                 new Soldier(i, 6, true, color, i);
             }
             new Rook(0, 7, true, color, 9);
@@ -61,7 +69,7 @@ public class Graphic {
             new Queen(3, 7, true, color, 12);
             new King(4, 7, true, color, 13);
         } else {
-            for (int i = 1; i <= 8; i++) {
+            for (int i = 0; i < 8; i++) {
                 new Soldier(i, 1, true, color, i);
             }
             new Rook(0, 0, true, color, 9);
@@ -76,6 +84,8 @@ public class Graphic {
     }
 
     private void paint(JButton[][] map, JPanel mainPanel){
+        mainPanel.removeAll();
+        mainPanel.revalidate();
         for (int i = 0; i < 8; i++){
             for (int j = 0; j < 8; j++) {
                 JButton btt = map[i][j];
@@ -88,5 +98,56 @@ public class Graphic {
                 mainPanel.add(btt);
             }
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (!clicked){
+            System.out.println("first");
+            if (((Piece)e.getSource()).getColor() == turn){
+                ((Piece) e.getSource()).setBackground(Color.GREEN);
+                clickedPiece = (Piece)e.getSource();
+                clicked = true;
+            }
+        } else {
+            System.out.println("sec");
+            JButton target = (JButton) e.getSource();
+            for (int i = 0; i < 8; i++){
+                for (int j = 0; j < 8; j++){
+                    if (map[i][j] == target){
+                        System.out.println("secsec");
+                        clickedPiece.move(j, i, map, true);
+                        paint(map, mainPanel);
+                    }
+                }
+            }
+            if (turn == 'W'){
+                turn = 'B';
+            } else {
+                turn = 'W';
+            }
+            clickedPiece = null;
+            clicked = false;
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
     }
 }
